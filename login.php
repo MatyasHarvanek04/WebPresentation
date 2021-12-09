876731ea0153bc701ce85e5127df48e43108e6db
HarviHarviHH <69310024+MatyasHarvanek04@users.noreply.github.com>
2021-12-09 18:25:32 +0100

    Login

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+    $logined = false;
+    include 'database.php';
+    if(isset($_COOKIE['Login']))
+    {
+        $sql = "SELECT `username`, `hash`  FROM `adminlogins` WHERE hash = '".$_COOKIE['Login']."'";
+        $result = mysqli_query($conn, $sql);
+        $logins = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        
+        if($logins)
+        {
+            $logined = true;
+            echo 'Prihlášen jako:'. $logins[0]['username'];
+           
+        }
+        
+    }
+    else
+    {
+        
+    }
+
+    
+?>
